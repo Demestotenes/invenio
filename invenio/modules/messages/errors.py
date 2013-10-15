6f5eeea6aabb7b15f5350f2b862fdd1b68b62dc8
Krzysztof Lis <krzysztof.lis@cern.ch>
2013-10-15 11:43:06 +0200

    errors: webmessage

--- invenio/modules/messages/errors.py
+++ invenio/modules/messages/errors.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 ##
 ## This file is part of Invenio.
-## Copyright (C) 2005, 2006, 2007, 2008, 2010, 2011 CERN.
+## Copyright (C) 2013 CERN.
 ##
 ## Invenio is free software; you can redistribute it and/or
 ## modify it under the terms of the GNU General Public License as
@@ -18,32 +18,18 @@
 ## 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
 
 """
-webmessage config file, here you can manage error messages, size of messages,
-quotas, and some db related fields...
+    invenio.modules.messages.errors
+    ---
+    Exceptions for Invenio Messages module
 """
 
-__revision__ = "$Id$"
 
-# status of message (table user_msgMESSAGE)
-CFG_WEBMESSAGE_STATUS_CODE = \
-{
-    'NEW': 'N',
-    'READ': 'R',
-    'REMINDER': 'M'
-}
-# values indicating which results field to display while writing a message
-CFG_WEBMESSAGE_RESULTS_FIELD = \
-{
-    'USER': 'user',
-    'GROUP': 'group',
-    'NONE': 'none'
-}
+class InvenioWebMessageError(Exception):
+    """A generic error for WebMessage."""
+    def __init__(self, message):
+        """Initialisation."""
+        self.message = message
 
-# separator used in every list of recipients
-CFG_WEBMESSAGE_SEPARATOR = ','
-
-# list of roles (find them in accROLE table) without quota
-CFG_WEBMESSAGE_ROLES_WITHOUT_QUOTA = ['superadmin']
-
-# alert user also by email
-CFG_WEBMESSAGE_EMAIL_ALERT = True
+    def __str__(self):
+        """String representation."""
+        return repr(self.message)
--- modules/webmessage/lib/webmessage.py
+++ modules/webmessage/lib/webmessage.py
@@ -25,8 +25,8 @@ import invenio.webmessage_dblayer as db
 from invenio.webmessage_config import CFG_WEBMESSAGE_STATUS_CODE, \
                                       CFG_WEBMESSAGE_RESULTS_FIELD, \
                                       CFG_WEBMESSAGE_SEPARATOR, \
-                                      CFG_WEBMESSAGE_ROLES_WITHOUT_QUOTA, \
-                                      InvenioWebMessageError
+                                      CFG_WEBMESSAGE_ROLES_WITHOUT_QUOTA
+from invenio.modules.messages.errors import InvenioWebMessageError
 from invenio.config import CFG_SITE_LANG, \
      CFG_WEBMESSAGE_MAX_SIZE_OF_MESSAGE
 from invenio.messages import gettext_set_language
